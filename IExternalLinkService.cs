0de66a6260e06b489bfbfae67cba7241cb401f5a
Melissa <m.dussek@gmail.com>
2021-11-29 18:40:22 -0800

    Add files via upload

---
+++ IExternalLinkService.cs
@@ -0,0 +1,21 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests;
+using Sabio.Models.Requests.ExternalLinks;
+using System;
+using System.Collections.Generic;
+
+namespace Sabio.Services.Interfaces 
+{
+    public interface IExternalLinkService
+    {
+        ExternalLink GetByCurrent(int userId);
+        List<ExternalLink> GetById(int id);
+
+        int Add(ExternalLinkAddRequest model);
+
+        void Delete(int id);
+
+        void Update(ExternalLinkUpdateRequest model);
+    }
+}
