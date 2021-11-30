0f40f54fce937339c553566ae76e4b627d9e46aa
Melissa <m.dussek@gmail.com>
2021-11-29 18:30:31 -0800

    Add files via upload

---
+++ ExternalLinkUpdateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.ExternalLinks
+{
+    public class ExternalLinkUpdateRequest : ExternalLinkAddRequest, IModelIdentifier
+    {
+        public int Id { get; set; }
+    }
+}
