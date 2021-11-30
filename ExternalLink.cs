8fb6bff693220f820782c0129e71b0ec41ddf353
Melissa <m.dussek@gmail.com>
2021-11-29 18:39:33 -0800

    Add files via upload

---
+++ ExternalLink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class ExternalLink
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int UrlTypeId { get; set; }
+        public string UrlType { get; set; }
+        public string Url { get; set; }
+        public int EntityId { get; set; }
+        public int EntityTypeId { get; set; }
+        public string UrlName { get; set; }
+        public string EntityType { get; set; }
+    }
+
+   }
+
+
+
+
